46e698c29e39dcb19da38132b972ca65febc206b
user <ghoust240@gmail.com>
2020-02-13 11:56:47 +0200

    130220_11_56

---
+++ classwork/python_section3_conditional.py
@@ -0,0 +1,3 @@
+x=int(input("Please, enter number: "))
+if x > 0 and x <10:
+    print('x is positive single-digit number.')
---
+++ classwork/python_section3_conditional4.py
@@ -0,0 +1,9 @@
+x=int(input("Please, enter number #1: "))
+y=int(input("Please, enter number #2: "))
+if x == y:
+    print('#1 and #2 are equal')
+else:
+    if x < y:
+        print('#1 is less than #2')
+    else:
+        print('#1 is greater than #2')
